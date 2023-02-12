489b1cb68932c630950f17a37913c1898f85e86e
E-Zbox <zboxstudios97@gmail.com>
2023-02-13 00:05:30 +0100

    added event listening functionality for create group message

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,13 +1,28 @@
 // event emitters and listeners
 const {
-    events: { CREATE_MESSAGE, GET_MESSAGES },
-    functions: { createMessage, getMessages },
+    events: {
+        CREATE_MESSAGE,
+        GET_MESSAGES,
+        CREATE_GROUP_MESSAGE,
+        GET_GROUP_MESSAGES,
+    },
+    functions: { createMessage, getMessages, createGroupMessage },
 } = require("./events/listeners");
 // socket middlewares
 const { addSocketToRoom } = require("../middlewares/message");
 
 exports.messageIOController = (io, socket) => {
+    // socket middleware
     socket.use((packet, next) => addSocketToRoom(packet, next, socket));
+
+    socket.emit("welcome_back_user", "It's great to have you here🤩");
+
     socket.on(CREATE_MESSAGE, (payload) => createMessage(payload, socket, io));
     socket.on(GET_MESSAGES, (payload) => getMessages(payload, socket));
+
+    // groups
+    socket.on(CREATE_GROUP_MESSAGE, (payload) =>
+        createGroupMessage(payload, socket, io)
+    );
+    console.log(createGroupMessage);
 };
